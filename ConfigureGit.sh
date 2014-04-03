d5f6c28fd6076cd342e533fb6a870af6d8e5101d
Cameron Allen <callen09@calpoly.edu>
2014-04-03 15:01:55 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+git config --global user.name "Cameron Allen"
+git config --global user.email "callen09@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
+
